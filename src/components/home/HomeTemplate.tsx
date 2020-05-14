f531be74c2167e5d75ba03930bc6990b077d1c05
Dotsung <dotsung22@gmail.com>
2020-05-15 04:37:23 +0900

    🚀 Init

---
+++ src/components/home/HomeTemplate.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+function HomeTemplate() {
+  return <Container>Sans</Container>;
+}
+
+export default HomeTemplate;
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import HomeTemplate from "components/home/HomeTemplate";
+
+function Home() {
+  return <HomeTemplate />;
+}
+
+export default Home;
